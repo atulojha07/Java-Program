a379cca6932669e7ea4dd98520f5b091b8c6f9c1
Atul kumar <143419578+atulojha07@users.noreply.github.com>
2025-01-12 10:31:14 +0530

    FactorialUsingRecursion.java

--- FactorialUsingRecursion.java
+++ FactorialUsingRecursion.java
@@ -1,1 +1,18 @@
-
+public class FactorialNumber {
+    static int recursion(int n){
+        if(n<0){
+            System.out.println("Not Possible");
+        }
+        if(n==1){
+            return 1;
+        }
+        return n*recursion(n-1);
+    }
+    public static void main(String args[]){
+        int fact=1;
+        for(int i=1; i<=5; i++){
+            fact=fact*i;
+        }
+        System.out.println(recursion(4));
+    }
+}
